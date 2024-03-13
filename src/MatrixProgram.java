9543283a8b1592c6c4b348cf1e1d0ed26e2f1dd5
cristic1988 <cristic8@gmail.com>
2024-03-13 22:45:49 +0200

    second commit

--- src/MatrixProgram.java
+++ src/MatrixProgram.java
@@ -9,16 +9,54 @@ public class MatrixProgram {
 
         System.out.println("Enter value M");
         int m = scanner.nextInt();
+        System.out.println(m);
+
+
+
         System.out.println("Enter value N");
         int n = scanner.nextInt();
+        System.out.println(n);
+
+
 
         int[][] array2DOriginal = new int[m][n];
         int[][] array2DTransported = new int[n][m];
-        for (int i = 0; i < array2DOriginal.length; i++){
-            for (int j = 0; j < array2DOriginal[i].length; j++){
+        for (int i = 0; i < array2DOriginal.length; i++) {
+            for (int j = 0; j < array2DOriginal[i].length; j++) {
                 array2DOriginal[i][j] = random.nextInt(10);
             }
         }
+        for (int i = 0; i < array2DOriginal.length; i++) {
+            for (int j = 0; j < array2DOriginal[i].length; j++) {
+                System.out.print(array2DOriginal[i][j] + " ");
+            }
+            System.out.println();
+            }
+        System.out.println();
+
+
+
+        for (int i = 0; i < array2DOriginal.length; i++) {
+            for (int j = 0; j < array2DOriginal[i].length; j++) {
+                int temp = array2DOriginal[i][j];
+                array2DOriginal[i][j] = array2DTransported[j][i];
+                array2DTransported[j][i] = temp;
+            }
+            }
+        for (int i = 0; i < array2DOriginal.length; i++) {
+            for (int j = 0; j < array2DOriginal[i].length; j++) {
+                System.out.print(array2DTransported[j][i] + " ");
+            }
+            System.out.println();
+            }
+
+      //  int temp = array2DOriginal[i][j];
+     //   array2DOriginal[i][j] = array2DTransported[j][i];
+     //   array2DTransported[j][i] = temp;
+
+
+
+
 
 
 
